cd157078e1399eef435e9d7121dc3512231a6397
JobsJr <rajeev.kr@practo.com>
2016-04-24 00:56:57 +0530

    add Platform POJO

---
+++ Where'sMyBus2/app/src/main/java/com/example/rajeevkr/wheresmybus/entity/Platform.java
@@ -0,0 +1,36 @@
+package com.example.rajeevkr.wheresmybus.entity;
+
+/**
+ * POJO that defines the data model
+ *
+ * Created by rajeevkr on 4/24/16.
+ */
+public class Platform {
+    private int mPlatformNum;
+    private String mBusNum;
+    private String mRouteAddress;
+
+    public String getRouteAddress() {
+        return mRouteAddress;
+    }
+
+    public void setRouteAddress(String mRouteAddress) {
+        this.mRouteAddress = mRouteAddress;
+    }
+
+    public int getPlatformNum() {
+        return mPlatformNum;
+    }
+
+    public void setPlatformNum(int mPlatformNum) {
+        this.mPlatformNum = mPlatformNum;
+    }
+
+    public String getBusNum() {
+        return mBusNum;
+    }
+
+    public void setBusNum(String mBusNum) {
+        this.mBusNum = mBusNum;
+    }
+}
