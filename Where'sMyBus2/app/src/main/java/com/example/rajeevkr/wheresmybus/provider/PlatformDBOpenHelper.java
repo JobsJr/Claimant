47c091880c6fb2a542b4b2c92c60a293e91b02d7
JobsJr <rajeev.kr@practo.com>
2016-05-22 13:42:41 +0530

    Add contract & db helper classes

---
+++ Where'sMyBus2/app/src/main/java/com/example/rajeevkr/wheresmybus/provider/PlatformDBOpenHelper.java
@@ -0,0 +1,58 @@
+package com.example.rajeevkr.wheresmybus.provider;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * DB Helper class
+ *
+ * @author rajeevkr
+ */
+public class PlatformDBOpenHelper extends SQLiteOpenHelper {
+    private static PlatformDBOpenHelper platFormDBOpenHelperInstanse;
+    private static final String DB_NAME = "platforms.db";
+    private static final String TABLE_NAME = "platforms";
+    private static final int DB_VERSION = 1;
+
+    private static final String CREATE_TABLE = "CREATE TABLE " +
+            TABLE_NAME + "( " +
+            PlatformInfoContract._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            PlatformInfoContract.COLUMN_BUS + " TEXT NOT NULL, " +
+            PlatformInfoContract.COLUMN_ROUTE + " TEXT NOT NULL, " +
+            PlatformInfoContract.COLUMN_PLATFORM + " INTEGER" + ");";
+
+    /***
+     * Since theres a one-one relation between DBHelper &
+     * DB Connnection,The SqliteDatabase object uses
+     * locks to keep access serialized.
+     * So, if 100 threads have one db instance,
+     * calls to the actual on-disk database are serialized.
+     */
+    public static PlatformDBOpenHelper getInstanse(Context context) {
+        if (platFormDBOpenHelperInstanse == null) {
+            //Singleton with double-checked locking
+            synchronized (PlatformDBOpenHelper.class) {
+                if (platFormDBOpenHelperInstanse == null) {
+                    platFormDBOpenHelperInstanse = new PlatformDBOpenHelper(context);
+                }
+            }
+        }
+        return platFormDBOpenHelperInstanse;
+    }
+
+    private PlatformDBOpenHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+}
---
+++ Where'sMyBus2/app/src/main/java/com/example/rajeevkr/wheresmybus/provider/PlatformInfoContract.java
@@ -0,0 +1,17 @@
+package com.example.rajeevkr.wheresmybus.provider;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by rajeevkr on 5/22/16.
+ */
+
+public class PlatformInfoContract implements BaseColumns {
+
+    /************ COLUMN INFO ***********/
+    public static final String COLUMN_BUS="bus";
+    public static final String COLUMN_ROUTE="route";
+    public static final String COLUMN_PLATFORM="platform";
+
+
+}
